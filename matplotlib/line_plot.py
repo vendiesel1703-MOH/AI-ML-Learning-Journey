c658591ec94a0f65aeaeff7549e8b819b4574626
MOHIT NAGDEEP <vendiesel.1703@gmail.com>
2026-06-17 09:01:54 +0530

    Add First Matplotlib line plot example

---
+++ matplotlib/line_plot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+plt.plot(x, y)
+plt.title("Simple Line Plot")
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+
+plt.show()
